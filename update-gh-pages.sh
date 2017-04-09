a968c84be8992400438388ab97f07c7eb4d9fdac
Utku Demir <utdemir@gmail.com>
2017-04-09 21:08:19 +1200

    Update update-gh-pages script

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -3,6 +3,7 @@
 set -o errexit
 
 stack haddock
+rev=$(git rev-parse HEAD)
 
 tmp=$(mktemp -d)
 cleanup() {
@@ -10,15 +11,17 @@ cleanup() {
 }
 trap cleanup EXIT
 
+git clone git@github.com:utdemir/apidoc-hs \
+    --branch gh-pages --single-branch --depth 1 \
+    "$tmp"
+
 cp -r "$(stack path --local-doc-root)"/* "$tmp"
 
 set -o xtrace
 
 cd "$tmp"
 
-git init
-git checkout -b gh-pages
 git add .
-git commit -m "Add haddock"
+git commit -m "Update haddock to $rev"
 git remote add origin git@github.com:utdemir/apidoc-hs
 git push --force --set-upstream origin gh-pages
